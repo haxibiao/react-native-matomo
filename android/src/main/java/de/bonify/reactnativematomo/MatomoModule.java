571e151a1f1e1474317dc8c515f16e336eaf6e1d
ivan <ivan@haxibiao.com>
2020-04-01 10:40:14 +0800

    add setDispatchInterval ReactMethod

--- android/src/main/java/de/bonify/reactnativematomo/MatomoModule.java
+++ android/src/main/java/de/bonify/reactnativematomo/MatomoModule.java
@@ -46,6 +46,11 @@ public class MatomoModule extends ReactContextBaseJavaModule implements Lifecycl
     }
 
     @ReactMethod
+    public void setDispatchInterval(long interval) {
+        mMatomoTracker.setDispatchInterval(interval);
+    }
+
+    @ReactMethod
     public void setCustomDimension(@NonNull int id, @Nullable String value){
         if(value == null || value.length() == 0) {
             customDimensions.remove(id);
